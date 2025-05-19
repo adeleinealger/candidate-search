ddac15057381d8862e55f5a38ee9ee5d0d3105ac
Adeleine Alger <adeleinealger@gmail.com>
2025-05-18 22:40:06 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   envDir: './environment',
   plugins: [react()],
+  preview: {
+    allowedHosts: ["candidate-search-3ytv.onrender.com"]
+  },
 });
